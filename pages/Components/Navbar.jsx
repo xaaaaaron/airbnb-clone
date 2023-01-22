ee4dc75670669facdc4cfe4438d202c760a19b16
xaaaron <aaron.alcarde@opensoftsolutions.com>
2023-01-22 21:14:07 +0800

    added calendar

--- pages/Components/Navbar.jsx
+++ pages/Components/Navbar.jsx
@@ -1,15 +1,37 @@
-import React from 'react'
+import React, {useEffect, useState } from 'react'
 import Image from 'next/image'
 import { MagnifyingGlassIcon, GlobeAltIcon, Bars3Icon } from '@heroicons/react/24/outline'
 import { UserCircleIcon } from '@heroicons/react/24/solid'
+import 'react-date-range/dist/styles.css'; // main style file
+import 'react-date-range/dist/theme/default.css'; // theme css file
+import { DateRangePicker } from 'react-date-range';
+import { endOfDecadeWithOptions } from 'date-fns/fp';
 
 
 const Navbar = () => {
+    const [searchInput, setSearchInput] = useState('')
+    const [startDate, setStartDate] = useState(new Date())
+    const [endDate, setEndDate] = useState(new Date())
+
+    const handleSelect = (ranges) => {
+        console.log(ranges)
+        setStartDate(ranges.selection.startDate)
+        setEndDate(ranges.selection.endDate)
+    }
+
+    const selectionRange = {
+        startDate: startDate,
+        endDate: endDate,
+        key: 'selection'
+    }
+
+   
+
     const logo = 'https://media.designrush.com/inspiration_images/135187/conversions/_1511452487_364_Airbnb-mobile.jpg'
     return (
         <>
         <header className="sticky top-0 z-50 grid grid-cols-3 bg-white shadow-md p-5 md:px-10">
-        {/* */}
+        {/* Left */}
         <div className="relative flex items-center h-12 cursor-pointer my-auto">
             <Image
                 alt="test"
@@ -24,14 +46,17 @@ const Navbar = () => {
         <div className="flex items-center md:border-2 rounded-full py-2 
         md:shadow-sm">
             <input className="flex-grow pl-5 bg-transparent outline-none text-sm text-gray-600" 
+            value={searchInput}
             type="text" 
-            placeholder="Start your search" />
+            placeholder="Start your search"
+            onChange={(e) => setSearchInput(e.target.value)}
+            />
             <MagnifyingGlassIcon className="hidden md:inline-flex h-8 bg-red-400 text-white 
                                             rounded-full p-2 cursor-pointer md:mx-2" 
                                             />
         </div>
 
-        {/* */}
+        {/* Right */}
         <div className="flex items-center space-x-4 justify-end text-gray-600">
             <p className="hidden md:inline">Airbnb your home</p>
             <GlobeAltIcon className="hidden h-6 md:inline" />
@@ -40,6 +65,17 @@ const Navbar = () => {
                 <UserCircleIcon className="h-6"/>
             </div>
         </div>
+
+        {searchInput && (
+            <div className="flex flex-col col-span-3 mx-auto pt-5">
+                <DateRangePicker
+                ranges={[selectionRange]}
+                minDate={new Date()}
+                rangeColors={["#FD5B61"]}
+                onChange={handleSelect} />
+            </div>
+        )}
+
         </header>
         </>
     )
