e24d7f0f69f681498604c4c3e5e05decb0389f7c
xaaaron <aaron.alcarde@opensoftsolutions.com>
2023-01-22 20:45:06 +0800

    added large cards

--- pages/Components/Navbar.jsx
+++ pages/Components/Navbar.jsx
@@ -34,8 +34,7 @@ const Navbar = () => {
         {/* */}
         <div className="flex items-center space-x-4 justify-end text-gray-600">
             <p className="hidden md:inline">Airbnb your home</p>
-            <GlobeAltIcon className="h-6" />
-
+            <GlobeAltIcon className="hidden h-6 md:inline" />
             <div className="flex items-center space-x-2 border-2 p-2 rounded-full cursor-pointer">
                 <Bars3Icon className="h-6"/>
                 <UserCircleIcon className="h-6"/>
--- pages/index.js
+++ pages/index.js
@@ -6,6 +6,7 @@ import Navbar from './Components/Navbar'
 import Banner from './Components/Banner'
 import { SmallCard } from './Components/SmallCard'
 import { MediumCards } from './Components/MediumCards'
+import { LargeCards } from './Components/LargeCards'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -43,6 +44,14 @@ export default function Home({ exploreData, cardsData }) {
             title={title}/>
         ))}
         </div>
+        </section>
+
+        <section>
+          <LargeCards
+          img="https://links.papareact.com/4cj"
+          title="The Greatest Outdoors"
+          description="By Airbnb"
+          buttonText="Get inspired" />
         </section>
       </main>
     </>
